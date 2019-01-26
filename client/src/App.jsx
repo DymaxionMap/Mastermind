080e7da2554838971b1118d45a4b6c427aa844ec
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-26 13:41:21 -0800

    Break article body into words

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ class App extends Component {
     super(props);
     this.state = {
       title: '',
-      body: '',
+      words: [],
     };
   }
 
@@ -13,17 +13,18 @@ class App extends Component {
     fetch('/articles/1')
       .then(data => data.json())
       .then((article) => {
-        const { title, body } = article;
-        this.setState({ title, body });
+        const { title, words } = article;
+        this.setState({ title, words });
       });
   }
 
   render() {
-    const { title, body } = this.state;
+    const { title, words, highlighted } = this.state;
     return (
       <div>
+        <h2>{highlighted}</h2>
         <h1>{title}</h1>
-        <p>{body}</p>
+        <p>{words.map((word, i) => <span key={i}>{`${word} `}</span>)}</p>
       </div>
     );
   }
