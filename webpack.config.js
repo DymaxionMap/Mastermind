aa6f935aa1720e16babefeb09d985b0456c119f1
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-26 11:38:35 -0800

    Install eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
-  module : {
+  module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
@@ -16,5 +17,8 @@ module.exports = {
         use: ['babel-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
